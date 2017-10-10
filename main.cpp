8ed4bae8cce3ab808bc6fd9941d4f0e48293de56
compusHGW <32165386+compusHGW@users.noreply.github.com>
2017-10-10 05:36:48 -0400

    Update main.cpp
    relative path for json-files

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ int main(int argc, char** argv) {
     auto hw_concurrency = 1;//std::thread::hardware_concurrency();
     for (int i = 0; i < hw_concurrency; ++i) {
         threads.emplace_back( [&]() {
-            auto simulation = read_simulation_file("/home/guba/CLionProjects/spuba/simulation.json");
+            auto simulation = read_simulation_file("../simulation.json");
             simulations.push_back( new Simulation(simulation) );
             {
                 auto& simulation = *simulations.back();
-                simulation.read_input_file("/home/guba/CLionProjects/spuba/input.json");
+                simulation.read_input_file("../input.json");
                 simulation.run();
             }
         });
